e50f52d780574385abe54903e695698b62f7c75c
Tim Rightnour <root@garbled.net>
2018-06-16 12:47:27 -0700

    Fiddle the python setup, document readme.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # py-gnhast
 Python module for gnhast
+
+This module allows you to build sensor-only collectors for gnhast,
+and script-type collectors for gnhast.  It implements most of the gnhast
+protocol.
+
+# Things that are missing:
+
+mod - Cannot handle a request to modify a device
+chg - Cannot handle a request to change a device
+alarms - No alarm support (yet)
+groups - No device group support.
+
+# Things that work
+* Easy to write sensor collectors.
+* Easy to write simple automation controls, for example, listen to a sensor, and then issue a command to gnhastd when something happens on that sensor.
+
+# Examples
+* See the https://github.com/garbled1/gnhast-python-collectors repo for collectors written using this module.
+
+# What is gnhast?
+
+Gnhast is an event based home automation suite of tools.
+It relies on a central daemon, which handles all the coordination of work,
+and collectors which handle all the actual work.
+Gnhast is designed to be run from a UNIX/Linux server,
+and is designed to be fairly lightweight, so it could easily
+be (and is!) deployed on a Raspbery PI to run your whole house.
+
+See the main gnhast repo at: https://github.com/garbled1/gnhast
--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='gnhast',
-    version='0.1',
+    version='0.1.1',
     description='Minimal code to create a gnhast collector',
     license='GPL',
     packages=['gnhast'],
     author='Tim Rightnour',
     author_email='thegarbledone@gmail.com',
     keywords=['homeautomation', 'ha', 'domotic', 'ghnast'],
-    url='https://github.com/garbled1/py-gnhast'
+    url='https://github.com/garbled1/py-gnhast',
+    project_urls={
+        'Gnhast API Documentation': 'https://garbled1.github.io/gnhast/',
+        'Gnhast Code Documentation': 'https://codedocs.xyz/garbled1/gnhast/',
+        'Gnhast Blog': 'http://gnhast.blogspot.com/',
+        'Gnhast Main Repo': 'https://github.com/garbled1/gnhast',
+        'Gnhast Python Collectors': 'https://github.com/garbled1/gnhast-python-collectors',
+    },
+    install_requires=[
+        'pint',
+        'ply'
+    ],
+    python_requires='>=3.5',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
